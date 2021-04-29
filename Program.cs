d72f7d52fee812e326a8269c7d6422eb8697899e
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-04-29 17:31:07 +0700

    homesat4.1

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace homesat_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int layer = int.Parse(Console.ReadLine());           
+            while (layer < 0) 
+            {
+                if (layer < 0) 
+                {
+                    Console.WriteLine(" Invalid Pascal's triangle row number.");
+
+                    int layer2 = int.Parse(Console.ReadLine());
+                    layer = layer2;                   
+                }
+            }
+            pas(layer);
+            
+            static void pas (int layer) 
+            {
+                int num = 0, i, j;
+                for (i = 0; i <= layer; i++) 
+                {
+                    for (j = 0; j <= i; j++) 
+                    {
+                        if (j == 0 || i == 0)
+                        {
+                            num = 1;
+                        }
+                        else 
+                        {
+                            num = num * ((i - j) + 1) / j;                       
+                        }
+                        Console.Write(num + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
